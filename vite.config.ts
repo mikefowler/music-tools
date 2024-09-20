65ce756cc4a1a41f0b9956f62bd50eff69be194e
Mike Fowler <mike@mikefowler.me>
2024-09-19 23:10:24 -0400

    Update build config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/making/music-tools',
+  root: './',
   build: {
     outDir: 'dist',
   },
+  base: '/making/music-tools',
+  publicDir: 'public',
   plugins: [react()],
 });
