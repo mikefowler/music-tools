cb18b5aec58abe6449bd081b35b81ddec5304471
Mike Fowler <mike@mikefowler.me>
2024-09-19 23:40:35 -0400

    Update build config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  let base = '.';
+
+  if (command === 'build') {
+    base = '/making/music-tools';
+  }
+
+  return {
+    plugins: [react()],
+    base,
+  };
 });
